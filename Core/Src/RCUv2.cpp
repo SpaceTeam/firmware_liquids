c02e25be6d39a7ef8401ddc21d683db18ea95dd9
theDiego21 <102720504+theDiego21@users.noreply.github.com>
2024-09-30 16:16:18 +0200

    Added 4 DIgitalOutChannels to RCU

--- Core/Src/RCUv2.cpp
+++ Core/Src/RCUv2.cpp
@@ -25,6 +25,10 @@ RCUv2::RCUv2(uint32_t node_id, uint32_t fw_version, uint32_t refresh_divider) :
 		gps_latitude(RCUv2_GNSS_LAT, &gnss.gnssData.latitude, 1),
 		gps_altitude(RCUv2_GNSS_ALT, &gnss.gnssData.altitude, 1),
 		gps_status(RCUv2_GNSS_STATUS, &gnss.gnssData.status, 1),
+		out0(RCUv2_OUT0, { GPIOA, 0, STRHAL_GPIO_TYPE_IHZ }, 1),
+		out1(RCUv2_OUT1, { GPIOC, 2, STRHAL_GPIO_TYPE_IHZ }, 1),
+		out2(RCUv2_OUT2, { GPIOC, 0, STRHAL_GPIO_TYPE_IHZ }, 1),
+		out3(RCUv2_OUT3, { GPIOBC, 10, STRHAL_GPIO_TYPE_IHZ }, 1),
 		radio(Radio::instance(node_id, lora)),
 		speaker(STRHAL_TIM_TIM2, STRHAL_TIM_TIM2_CH3_PB10)
 {
@@ -45,6 +49,10 @@ RCUv2::RCUv2(uint32_t node_id, uint32_t fw_version, uint32_t refresh_divider) :
 	registerChannel(&gps_latitude);
 	registerChannel(&gps_altitude);
 	registerChannel(&gps_status);
+	registerChannel(&out0);
+	registerChannel(&out1);
+	registerChannel(&out2);
+	registerChannel(&out3);
 
 	registerModule(&flash);
 	registerModule(&gnss);
@@ -117,6 +125,7 @@ int RCUv2::exec()
 	while (1)
 	{
 		detectReadoutMode();
+		testIMU();
 #ifdef UART_DEBUG
 
 		uint8_t tempBuf[64] =

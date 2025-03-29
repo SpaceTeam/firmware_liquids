6d9f1c42fa28672359fec049031aec373aa22e4d
Michael Debertol <michael.debertol@gmail.com>
2025-03-29 17:56:31 +0100

    buffer can messages to avoid interrupting other code
    Because CAN messages can arrive at any time, they might interrupt code in a critical section and cause reentrancy issues.
    
    With this change, they will be handled by the main loop, with the interrupt handler only adding them to a buffer.
    
    After discussion with Raffael Rott the code was moved to Can.cpp

---
+++ Core/Inc/RingBuf.h
@@ -0,0 +1,38 @@
+#ifndef FIRMWARE_LIQUIDS_RINGBUF_H
+#define FIRMWARE_LIQUIDS_RINGBUF_H
+
+#include <atomic>
+
+template<typename T, uint8_t SIZE>
+class RingBuf {
+public:
+    void push(T value) {
+        if (bufSize == SIZE) {
+            return; // The buffer is already full.
+        }
+        buf[bufWriteIndex] = value;
+        bufWriteIndex = (bufWriteIndex + 1) % SIZE;
+        ++bufSize;
+    }
+
+
+    T pop() {
+        T value = buf[bufReadIndex];
+        bufReadIndex = (bufReadIndex + 1) % SIZE;
+        --bufSize;
+        return value;
+    }
+
+    [[nodiscard]] bool isEmpty() const {
+        return bufSize == 0;
+    }
+
+private:
+    T buf[SIZE] = {};
+    volatile uint8_t bufReadIndex = 0;
+    volatile uint8_t bufWriteIndex = 0;
+    std::atomic<uint8_t> bufSize = 0;
+};
+
+
+#endif // FIRMWARE_LIQUIDS_RINGBUF_H
